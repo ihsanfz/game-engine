27c548d436f567876d79ed8af26666b690e5d8e2
Ihsan Fazil <ihsaninku@gmail.com>
2025-08-13 23:27:08 +0530

    Texture layering

--- game-engine.cpp
+++ game-engine.cpp
@@ -72,7 +72,8 @@ int main()
 	glEnableVertexAttribArray(2);
 
 
-	unsigned int texture1;
+	unsigned int texture1, texture2;
+
 	glGenTextures(1, &texture1);
 	glBindTexture(GL_TEXTURE_2D, texture1);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
@@ -93,11 +94,29 @@ int main()
 		std::cout << "Failed to load texture" << std::endl;
 	}
 	stbi_image_free(data);
+
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	data = stbi_load("C:/Users/ihsan/Downloads/test.png", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
 	
 	newShader.use();
 	glUniform1i(glGetUniformLocation(newShader.ID, "texture1"), 0);
-
-	glViewport(0, 0, 800, 600);
+	newShader.setInt("texture2", 1);
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
@@ -111,6 +130,8 @@ int main()
 
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, texture1);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture2);
 		
 		newShader.use();
 		glBindVertexArray(VAO);
